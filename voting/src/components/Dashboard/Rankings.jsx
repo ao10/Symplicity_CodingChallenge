00db30f475a4d7bccee68273d8f947dc4be8098c
Ati Ok <ao10@umbc.edu>
2018-02-16 00:34:56 -0500

    Added Procfile for Heroku deployment

--- voting/src/components/Dashboard/Rankings.jsx
+++ voting/src/components/Dashboard/Rankings.jsx
@@ -7,6 +7,7 @@ export default class Rankings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            rankingData: null,
 
         }
     }
@@ -14,16 +15,23 @@ export default class Rankings extends React.Component {
     componentDidMount(){
         axios.get("http://localhost:8080/rankings").then(
             response => {
-                console.log(response)
+//                console.log(response)
+                var sortResponse = response.data;
+//                sortResponse.sort(function(a,b){
+//                    if(a.numVotes < b.numVotes)
+//                        return -1;
+//                 });
+                console.log(sortResponse);
+                this.setState({rankingData: sortResponse})
             })
     }
 
     render() {
         return (
             <List>                
-                <ListItem primaryText="Apple" />
+                <ListItem primaryText={this.state.rankingData[0].fruitName}   />
                 <ListItem primaryText="Banana"  />
-                <ListItem primaryText="Orange" />
+                <ListItem primaryText="Orange"  />
                 <ListItem primaryText="Pineapple" />
             </List>
         )
